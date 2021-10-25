14e8734efb11eaa9e838f3c32fda1ff5eedd1ddf
IUrii Belonozhkin <ibelonozhkin@neoflex.ru>
2021-10-25 10:51:31 +0300

    Initial commit

---
+++ 01.hello-world/helloworld-extended.go
@@ -0,0 +1,11 @@
+// проба пера 31 августа 2021
+package main
+
+import "fmt"
+
+var i int
+
+func main() {
+	i = 5
+	fmt.Println("Hello, мир:", i)
+}
---
+++ 01.hello-world/helloworld.go
@@ -0,0 +1,10 @@
+// первая программка 15 августа 2021
+package main
+
+import "fmt"
+
+
+
+func main() {
+	fmt.Println("Hello, мир")
+}
